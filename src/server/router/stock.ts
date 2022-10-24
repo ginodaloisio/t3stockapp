80ed0e6ede3e6d6373ef98ca710fdea58ad5a294
appeL <ginodaloisio@icloud.com>
2022-10-24 20:18:02 -0300

    refactored some logic + hability to get a single item from db

--- src/server/router/stock.ts
+++ src/server/router/stock.ts
@@ -21,6 +21,19 @@ export const stockRouter = createRouter()
       return await ctx.prisma.post.findMany();
     },
   })
+  .query("getItem", {
+    input: z.object({
+      id: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const result = await ctx.prisma.post.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+      return result;
+    },
+  })
   .query("searchItems", {
     input: z.object({
       searchString: z.string(),
@@ -66,15 +79,46 @@ export const stockRouter = createRouter()
         data: {
           title: input.title,
           content: input.content,
+          type: input?.type,
+          brand: input.brand,
+          height: input?.height,
+          length_: input?.length_,
+          width: input?.width,
+          image: input.imageURL,
+          author: { connect: { email: input.authorEmail } },
+        },
+      });
+      return item.id;
+    },
+  })
+  .mutation("editItem", {
+    input: z.object({
+      id: z.string(),
+      title: z.string(),
+      content: z.string(),
+      type: z.string().nullish(),
+      brand: z.nativeEnum(Brands),
+      height: z.number().nullish(),
+      length_: z.number().nullish(),
+      width: z.number().nullish(),
+      image: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const updatedItem = await ctx.prisma.post.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+          content: input.content,
           type: input.type != null ? input.type : undefined,
           brand: input.brand,
           height: input.height != null ? input.height : undefined,
           length_: input.length_ != null ? input.length_ : undefined,
           width: input.width != null ? input.width : undefined,
-          image: input.imageURL,
-          author: { connect: { email: input.authorEmail } },
+          image: input.image,
         },
       });
-      return item;
+      return updatedItem;
     },
   });
