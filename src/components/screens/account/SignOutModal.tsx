200f421e3e69a138ca1eaede8b6393e12d3dea37
appeL <ginodaloisio@icloud.com>
2022-12-05 21:26:00 -0300

    more buttons changes

--- src/components/screens/account/SignOutModal.tsx
+++ src/components/screens/account/SignOutModal.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonVariant } from "../../common/Button/Button";
+import { Button, Variant } from "../../common/Button/Button";
 import { Modal, ModalActions } from "../../common/Modal";
 
 export const SignOutModal = ({
@@ -26,17 +26,13 @@ export const SignOutModal = ({
     >
       <ModalActions>
         <Button
-          variant={ButtonVariant.Secondary}
+          variant={Variant.Secondary}
           onClick={handleCancel}
           type="button"
         >
           Cancelar
         </Button>
-        <Button
-          variant={ButtonVariant.Primary}
-          onClick={onConfirm}
-          type="button"
-        >
+        <Button variant={Variant.Danger} onClick={onConfirm} type="button">
           Cerrar
         </Button>
       </ModalActions>
