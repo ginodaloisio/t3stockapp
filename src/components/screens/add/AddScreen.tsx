94b358d88093da337ddb549edf34ce320fec3f73
appeL <ginodaloisio@icloud.com>
2022-12-05 21:26:20 -0300

    using user id instead of email

--- src/components/screens/add/AddScreen.tsx
+++ src/components/screens/add/AddScreen.tsx
@@ -2,14 +2,14 @@ import { useAddItem } from "./hooks/useAddItem";
 import { AddForm } from "./AddForm";
 
 //FIXME: el select en smaller devices se ve mal
-const AddScreen = ({ authorEmail }: { authorEmail: string }) => {
+const AddScreen = ({ authorId }: { authorId: string }) => {
   const { isLoading, showError, handleAddItemComplete } = useAddItem();
   return (
     <AddForm
       showError={showError}
       isLoading={isLoading}
       onComplete={handleAddItemComplete}
-      authorEmail={authorEmail}
+      authorId={authorId}
     />
   );
 };
