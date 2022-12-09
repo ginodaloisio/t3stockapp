72b2949a3cdb1d25ece764a3fbae15456aec5c6e
appeL <ginodaloisio@icloud.com>
2022-12-08 22:48:38 -0300

    was missing form to add prices

--- src/components/common/PriceHistory/EmptyStatePriceHistory.tsx
+++ src/components/common/PriceHistory/EmptyStatePriceHistory.tsx
@@ -1,7 +1,30 @@
-export const EmptyStatePriceHistory = () => {
+import { AddPriceForm } from "./AddPriceForm";
+import { useAddPrice } from "./hooks/useAddPrice";
+
+export const EmptyStatePriceHistory = ({
+  authorId,
+  refreshPrices,
+  postId,
+}: {
+  authorId: string;
+  postId: string;
+  refreshPrices: Function;
+}) => {
+  const { isLoading, showError, handleAddPriceComplete } = useAddPrice({
+    refreshPrices,
+  });
   return (
     <div className="mx-auto items-center justify-center gap-8">
-      <div className="text-2xl">Este articulo no tiene precio todavia!</div>
+      <span className="text-2xl">Este articulo no tiene precio todavia!</span>
+      <div className="mt-8">
+        <AddPriceForm
+          isLoading={isLoading}
+          showError={showError}
+          onComplete={handleAddPriceComplete}
+          authorId={authorId}
+          postId={postId}
+        />
+      </div>
     </div>
   );
 };
