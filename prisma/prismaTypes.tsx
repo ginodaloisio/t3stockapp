fa94629f55f4b9ca29a74af5661424752f3979e7
appeL <ginodaloisio@icloud.com>
2022-12-08 22:42:24 -0300

    changing file name

---
+++ prisma/prismaTypes.tsx
@@ -0,0 +1,43 @@
+import type { Brands as PrismaBrands } from "@prisma/client";
+import type { Post as PrismaPost } from "@prisma/client";
+
+//We need to map all prisma enums to objects so they can be safely imported into client side code
+// Otherwise this will break the build by importing the WHOLE Prisma client on the client side
+// see https://github.com/prisma/prisma/issues/12504
+
+export const Brands: { [k in PrismaBrands]: k } = {
+  GENERAL: "GENERAL",
+  VALENZIANA: "VALENZIANA",
+  FRANCOVALENTE: "FRANCOVALENTE",
+  SIMMONS: "SIMMONS",
+  CANNON: "CANNON",
+} as const;
+
+export type Brands = PrismaBrands;
+
+export type Post = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  brand: Brands;
+  title: string;
+  content: string;
+  type: string | null;
+  height: number | null;
+  width: number | null;
+  length_: number | null;
+  images: Images[];
+  amount: number;
+  published: boolean;
+  viewCount: number;
+  authorId: string | null;
+};
+
+export type Images = {
+  id: string;
+  title: string | null;
+  url: string;
+  createdAt: Date;
+  authorId: string;
+  postId: string;
+};
