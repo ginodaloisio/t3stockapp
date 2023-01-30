b1d32333466882077c50037fe9393023359969bd
appeLL-32 <65517605+appeLL-32@users.noreply.github.com>
2023-01-30 15:56:16 -0300

    fixing issue introduced with last commit (#10)
    * fixing issue introduced with last commit

--- src/components/common/Items/Results.tsx
+++ src/components/common/Items/Results.tsx
@@ -1,10 +1,14 @@
 // import { Post } from "../../../../prisma/prismaTypes";
 // function ItemRender({ results }: { results: Post[] }) {
 
-import { Images, Post } from "@prisma/client";
+import { Images, Post, Prices } from "@prisma/client";
 import Items from "./components/Items";
 
-function ItemRender({ results }: { results: (Post & { images: Images[] })[] }) {
+function ItemRender({
+  results,
+}: {
+  results: (Post & { images: Images[]; prices: Prices })[];
+}) {
   return (
     <div className="3xl:flex my-10 flex-wrap justify-center px-5 sm:grid md:grid-cols-2 xl:grid-cols-3">
       {results.map((result) => (
